982b04c1e4dce72700667970bb1054068efba006
fearlessyyp <794306705@qq.com>
2016-06-07 18:01:07 +0800

    Bcontroller

---
+++ Test1/Test1/BViewController.m
@@ -0,0 +1,37 @@
+//
+//  BViewController.m
+//  Test1
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 yyp. All rights reserved.
+//
+
+#import "BViewController.h"
+
+@interface BViewController ()
+
+@end
+
+@implementation BViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
